acd7ea5e98c4a2de63e356e77008fe3f070d94d3
Matheus Ferreira <matheusfdantas03@gmail.com>
2017-12-21 13:46:26 -0200

    adicionado getters e setters

--- src/br/com/triadworks/todolist/modelo/Tarefa.java
+++ src/br/com/triadworks/todolist/modelo/Tarefa.java
@@ -1,9 +1,37 @@
 package br.com.triadworks.todolist.modelo;
 
 public class Tarefa {
-	
+
 	private Integer id;
 	private String descricao;
 	private Usuario usuario;
 	
+	public Tarefa() {
+		
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
 }
--- src/br/com/triadworks/todolist/modelo/Usuario.java
+++ src/br/com/triadworks/todolist/modelo/Usuario.java
@@ -5,5 +5,33 @@ public class Usuario {
 	private Integer id;
 	private String login;
 	private String senha;
-	
+
+	public Usuario() {
+
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
 }
