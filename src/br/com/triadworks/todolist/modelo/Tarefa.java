9b8511cd71bf7d41cb0e02d43960f9d20f2b5c5b
Matheus Ferreira <matheusfdantas03@gmail.com>
2017-12-21 13:41:04 -0200

    criado o modelo usuario

--- src/br/com/triadworks/todolist/modelo/Tarefa.java
+++ src/br/com/triadworks/todolist/modelo/Tarefa.java
@@ -1,5 +1,9 @@
 package br.com.triadworks.todolist.modelo;
 
 public class Tarefa {
-
+	
+	private Integer id;
+	private String descricao;
+	private Usuario usuario;
+	
 }
---
+++ src/br/com/triadworks/todolist/modelo/Usuario.java
@@ -0,0 +1,9 @@
+package br.com.triadworks.todolist.modelo;
+
+public class Usuario {
+
+	private Integer id;
+	private String login;
+	private String senha;
+	
+}
